e7c4e9001ded7a040cabed9fc49f85d0a13634ff
gaturuHarry95 <gaturuharrison@gmail.com>
2023-11-09 23:02:53 +0300

    modified files

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,27 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const fullName='Harrison Maina';
+  const educationBackground='Bachelors Degree in Agriculture and Enterprise Development';
+  const professionalBackground='Front-End Development at Azubi Africa'
+  const skills=['HTML','CSS','JavaScript','ReactJs']
+
 
   return (
     <>
       <h1>Hello World</h1>
-      <p>I am Harrison Maina.This is my first React application</p>
+      <h1>{fullName}</h1>
+      <h2>Education Background</h2>
+        <p>{educationBackground}</p>
+        <h2>Professional Background</h2>
+        <p>{professionalBackground}</p>
+        <h2>Skills</h2>
+     <ul>
+     {skills.map((skill,index)=>(
+      <li key={index}>{skill}</li>
+     
+))}
+</ul>
     </>
   )
 }
