2bd4616b35099c602aafb75fac6cdeba1d68794f
gaturuHarry95 <gaturuharrison@gmail.com>
2023-11-12 16:46:37 +0300

    modified files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './Components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,12 @@ function App() {
   const educationBackground='Bachelors Degree in Agriculture and Enterprise Development';
   const professionalBackground='Front-End Development at Azubi Africa'
   const skills=['HTML','CSS','JavaScript','ReactJs']
+
+  const styles={
+    backgroundColor:'blue'
+    
+  }
+
 
 
   return (
@@ -26,6 +33,11 @@ function App() {
      
 ))}
 </ul>
+<div class='btn'>
+  <Button style={styles} name='Login'/>
+<Button style={styles} name='Sign in'/>
+</div>
+
     </>
   )
 }
---
+++ src/Components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <div>
+      <button style={props.style}>{props.name}</button>
+      
+      
+    </div>
+  )
+}
+
+export default Button
